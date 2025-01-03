9fc67eeba220621f3c780c44f72103740ac18cd5
Wilson Chen <wilson7chen@gmail.com>
2025-01-03 15:15:16 -0500

    Added Valid_Sudoku_36

--- src/Valid_Sudoku_36.java
+++ src/Valid_Sudoku_36.java
@@ -1,24 +1,39 @@
 import java.util.HashSet;
 import java.util.Set;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Valid_Sudoku_36 {
     public static boolean isValidSudoku(char[][] board) {
-        boolean result = true;
-        Set<Character> set = new HashSet<Character>();
+        Set<String> set = new HashSet<>();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                
+                if (board[i][j] != '.') {
+                    if(set.contains("r" + i + board[i][j]) || set.contains("c" + j + board[i][j]) || set.contains("b" + ((i / 3) * 3 + j / 3) + board[i][j])) {
+                        return false;
+                    } else {
+                        set.add("r" + i + board[i][j]);
+                        set.add("c" + j + board[i][j]);
+                        set.add("b" + ((i / 3) * 3 + j / 3) + board[i][j]);
+                    }
+                }
             }
         }
-
-        return result;
+        return true;
     }
 
     public static void main(String[] args) {
+//        char[][] board =
+//            {{'5','3','.','.','7','.','.','.','.'}
+//            ,{'6','.','.','1','9','5','.','.','.'}
+//            ,{'.','9','8','.','.','.','.','6','.'}
+//            ,{'8','.','.','.','6','.','.','.','3'}
+//            ,{'4','.','.','8','.','3','.','.','1'}
+//            ,{'7','.','.','.','2','.','.','.','6'}
+//            ,{'.','6','.','.','.','.','2','8','.'}
+//            ,{'.','.','.','4','1','9','.','.','5'}
+//            ,{'.','.','.','.','8','.','.','7','9'}}; // true
+
         char[][] board =
-            {{'5','3','.','.','7','.','.','.','.'}
+            {{'8','3','.','.','7','.','.','.','.'}
             ,{'6','.','.','1','9','5','.','.','.'}
             ,{'.','9','8','.','.','.','.','6','.'}
             ,{'8','.','.','.','6','.','.','.','3'}
@@ -26,7 +41,7 @@ public class Valid_Sudoku_36 {
             ,{'7','.','.','.','2','.','.','.','6'}
             ,{'.','6','.','.','.','.','2','8','.'}
             ,{'.','.','.','4','1','9','.','.','5'}
-            ,{'.','.','.','.','8','.','.','7','9'}};
+            ,{'.','.','.','.','8','.','.','7','9'}}; // false
 
         System.out.println(isValidSudoku(board));
     }
